a65eb608a14c179ae848c91f18a0a27c60d24b6d
Joseph Khawly <jkhawly@mouthwateringmedia.com>
2022-03-28 22:39:30 -0600

    Layout, date/time display

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,59 +1,33 @@
 <script context="module">
-	export const prerender = true;
+	export const prerender = true
 </script>
 
 <script>
-	import Counter from '$lib/Counter.svelte';
+	import { onMount } from 'svelte'
+	import dayjs from 'dayjs'
+	$: timeString = () => dayjs().format('hh:mm:ss A')
+	$: dateString = () => dayjs().format('ddd MMM DD')
+	let dateTime = ''
+
+	let termInput
+
+	function startTime() {
+		dateTime = `${dateString()} ${timeString()}`
+		setTimeout(startTime, 500)
+	}
+
+	onMount(() => {
+		startTime()
+		termInput.focus()
+	})
 </script>
 
 <svelte:head>
 	<title>~</title>
 </svelte:head>
 
-<section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<div class="terminal" on:click={() => termInput.focus()}>
+	<p class="prompt">joseph@mac:$&nbsp;</p>
+	<pre class="input" contenteditable="true" spellcheck="false" bind:this={termInput} />
+</div>
+<div class="clock">{dateTime}</div>
