52508b2b7a19c09e2bda1712dd407eb60f89c73e
Joseph Khawly <jkhawly@mouthwateringmedia.com>
2022-03-29 22:19:12 -0600

    command output and history

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
-<script context="module">
+<!-- <script context="module">
 	export const prerender = true
-</script>
+</script> -->
 
 <script>
 	import { onMount } from 'svelte'
@@ -10,6 +10,9 @@
 	$: timeString = () => dayjs().format('hh:mm:ss A')
 	$: dateString = () => dayjs().format('ddd MMM DD')
 	let dateTime = ''
+	let history = []
+	let lineData = []
+	let histIndex = 0
 
 	let termInput
 
@@ -19,10 +22,32 @@
 	}
 
 	function handleKeypress(e) {
+		const command = termInput.value
 		if (e.key === 'Enter') {
 			e.preventDefault()
-			const output = handle(termInput.value)
+			const output = handle(command)
+			lineData[lineData.length] = { command, output }
 			termInput.value = ''
+
+			if (command === '' || /^[ ]+$/.test(command) || history[0] === command) return
+
+			history[history.length] = command
+			// histIndex = history.length
+		} else if (e.key === 'ArrowUp') {
+			e.preventDefault()
+			if (histIndex < history.length) {
+				histIndex++
+				termInput.value = history[history.length - histIndex]
+			}
+		} else if (e.key === 'ArrowDown') {
+			e.preventDefault()
+			if (histIndex > 1) {
+				histIndex--
+				termInput.value = history[history.length - histIndex]
+			} else {
+				histIndex = 0
+				termInput.value = ''
+			}
 		}
 	}
 
@@ -37,6 +62,14 @@
 </svelte:head>
 
 <div class="terminal" on:click={() => termInput.focus()}>
+	{#each lineData as line, i (i)}
+		<span>
+			<p class="prompt">joseph@mac:$&nbsp;</p>
+			<pre class="input-old">{line.command}</pre>
+			<br />
+			<pre class="output">{line.output}</pre>
+		</span>
+	{/each}
 	<p class="prompt">joseph@mac:$&nbsp;</p>
 	<input
 		class="input"
