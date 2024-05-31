c46a1d73591be6e20fef4462db8ae755bc9712e8
Joseph Khawly <joseph.khawly@gmail.com>
2024-05-30 18:43:43 -0600

    upgrade to svelte 4 and sveltekit 2

--- svelte.config.js
+++ svelte.config.js
@@ -4,11 +4,6 @@ import adapter from '@sveltejs/adapter-auto';
 const config = {
 	kit: {
 		adapter: adapter(),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
 	}
 };
 
